e4c292e60ba8144da1b82a333bd430827fb325f1
ThomasP <legends.legardus@gmail.com>
2024-01-22 18:42:23 +0100

    update astro et ajout mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3000
   },
-})
+  integrations: [mdx()]
+});
